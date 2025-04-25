a2cbcec459add7032574223d86a40ccaa0cf536c
Norihiro Narayama <45268313+northprint@users.noreply.github.com>
2025-04-25 19:23:00 +0900

    実験

--- get-satellite-image/cdk/bin/cdk.ts
+++ get-satellite-image/cdk/bin/cdk.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-// import { CdkStack } from '../lib/cdk-stack';
+import { CdkStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
-new cdk.Stack(app, "EmptyStackForTest", {
+new CdkStack(app, "CdkStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
